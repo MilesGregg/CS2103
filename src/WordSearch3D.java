3387b9dfa3bf68623f4effedfc5a9354988ef8a6
Miles <milesgregg3@gmail.com>
2020-10-24 20:45:15 -0400

    initial push

---
+++ src/WordSearch3D.java
@@ -0,0 +1,394 @@
+import java.util.Random;
+import java.util.*;
+import java.io.*;
+
+/**
+ * Implements a 3-d word search puzzle program.
+ */
+public class WordSearch3D {
+	public WordSearch3D () {
+	}
+
+	/**
+	 * Searches for all the words in the specified list in the specified grid.
+	 * You should not need to modify this method.
+	 * @param grid the grid of characters comprising the word search puzzle
+	 * @param words the words to search for
+	 * @param a list of lists of locations of the letters in the words
+	 */
+	public int[][][] searchForAll (char[][][] grid, String[] words) {
+		final int[][][] locations = new int[words.length][][];
+		for (int i = 0; i < words.length; i++) {
+			locations[i] = search(grid, words[i]);
+		}
+		return locations;
+	}
+
+	/**
+	 * Searches for the specified word in the specified grid.
+	 * @param grid the grid of characters comprising the word search puzzle
+	 * @param word the word to search for
+	 * @return If the grid contains the
+	 * word, then the method returns a list of the (3-d) locations of its letters; if not, 
+	 */
+	/*public int[][] search (char[][][] grid, String word) {
+		// TODO: implement me
+		int[] xPos = { -1, -1, -1, 0, 0, 1, 1, 1, 0 };
+		int[] yPos = { -1, 0, 1, -1, 1, -1, 0, 1, 0 };
+		int[] zPos = { 1, 0, -1 };
+
+		int xLen = grid[0][0].length;
+		int yLen = grid[0].length;
+		int zLen = grid.length;
+
+		System.out.println("xLen: " + xLen + "\nyLen: " + yLen + "\nzLen: " + zLen);
+
+		int len = word.length();
+
+		for (int y = 0; y < yLen; y++) {
+			for (int x = 0; x < xLen; x++) {
+				for (int z = 0; z < zLen; z++) {
+					System.out.println("letter: " + grid[z][y][x]);
+					if (grid[z][y][x] != word.charAt(0)) {
+						continue;
+					}
+					int[] firstFound = new int[] { z, y, x };
+					System.out.println("First found: " + Arrays.toString(firstFound));
+					System.out.println("First Letter found: " + word.charAt(0));
+
+					for (int j = 0; j < 3; j++) {
+						for (int dir = 0; dir < 9; dir++) {
+							int i;
+							int xDir = x + xPos[dir];
+							int yDir = y + yPos[dir];
+							int zDir = zPos[j];
+
+							//System.out.println("Current x checking: " + xDir);
+							//System.out.println("Current y checking: " + yDir);
+							//System.out.println("Current z checking: " + zDir);
+
+							// first letter is found so lets look in different direction and keep looking if all the letters are there
+							for (i = 1; i < len; i++) {
+								// out of bounds!
+								if (xDir >= xLen || xDir < 0 || yDir >= yLen || yDir < 0 || zDir >= zLen || zDir < 0) {
+									//System.out.println("out of bounds!");
+									break;
+								}
+
+								System.out.println("next letter: " + grid[zDir][yDir][xDir]);
+
+								if (grid[zDir][yDir][xDir] != word.charAt(i))
+									break;
+
+								if (word.charAt(word.length()-1) == i) {
+									xDir += xPos[dir];
+									yDir += yPos[dir];
+								}
+								//zDir += zPos[j];
+							}
+
+							if (i == len) {
+								int[] lastFound = new int[] { zDir, yDir, xDir };
+								System.out.println("Last found: " + Arrays.toString(lastFound));
+								System.out.println("FOUND!");
+
+								int[][] test = new int[][] { firstFound, lastFound };
+
+								System.out.println("Found pos: " + Arrays.deepToString(test));
+
+								return new int[][] { firstFound, lastFound };
+							}
+						}
+					}
+
+					/*for (int dir = 0; dir < 8; dir++) {
+						for (int j = 0; j < 3; j++) {
+							int i;
+							int xDir = x + xPos[dir];
+							int yDir = y + yPos[dir];
+							int zDir = z + zPos[j];
+
+							// first letter is checked so keep checking
+							for (i = 1; i < len; i++) {
+								// out of bounds!
+								if (xDir >= xLen || xDir < 0 || yDir >= yLen || yDir < 0 || zDir >= zLen || zDir < 0) {
+									break;
+								}
+
+								System.out.println("Current x checking: " + xDir);
+								System.out.println("Current y checking: " + yDir);
+								System.out.println("Current z checking: " + zDir);
+								System.out.println("WTF: " + grid[zDir][xDir][yDir]);
+
+								if (grid[zDir][xDir][yDir] != word.charAt(i)) {
+									System.out.println("HERE");
+									break;
+								}
+
+								System.out.println("Before: " + xDir);
+								xDir += xPos[dir];
+								yDir += yPos[dir];
+								zDir += zPos[j];
+								System.out.println("After: " + xDir);
+							}
+
+							if (i == len) {
+								int[] lastFound = new int[] { xDir, yDir, zDir };
+								System.out.println("Last found: " + Arrays.toString(lastFound));
+								System.out.println("FOUND!");
+
+								int[][] test = new int[][] { firstFound, lastFound };
+
+								System.out.println("Found pos: " + Arrays.deepToString(test));
+
+								return new int[][] { firstFound, lastFound };
+							}
+						}
+					}*/
+				/*}
+			}
+		}
+
+		return null;
+	}*/
+
+	/**
+	 * Creates a list of 3-dimensional vectors where each component of each vector is
+	 * between -1 and 1, and all 3 components cannot be zero (total of 26 vectors)
+	 * @return the list of vectors
+	 */
+	private ArrayList<int[]> makeVectors(){
+		ArrayList<int[]> o = new ArrayList<>();
+		for(int i = -1; i<=1; i++){
+			for(int j = -1; j <= 1; j++){
+				for(int k = -1; k <= 1; k++){
+					if(i==0 && j==0 && k==0) continue;
+					o.add(new int[] {i, j, k});
+				}
+			}
+		}
+		return o;
+	}
+
+	/**
+	 * Given a starting position in the grid and a vector, checks if you can obtain the desired word
+	 * by starting at the start point and moving in the direction of the vector
+	 * @param grid the grid that is being searched
+	 * @param word the word that is being searched for
+	 * @param vector the vector signaling which direction from the starting point to look
+	 * @param i the x-component of the current position in the grid
+	 * @param j the y-component of the current position in the grid
+	 * @param k the z-component of the current position in the grid
+	 * @return a list of the coordinates for each point in the word if the word can be found with
+	 * the initial conditions specified, null otherwise
+	 */
+	private int[][] checkVector(char[][][] grid, String word, int[] vector, int i, int j, int k){
+		int xdir = vector[0];
+		int ydir = vector[1];
+		int zdir = vector[2];
+		StringBuilder newWord = new StringBuilder();
+		for(int n = 0; n < word.length(); n++){
+			char ch = grid[i + xdir*n][j+ydir*n][k+zdir*n];
+			newWord.append(ch);
+		}
+		//	System.out.println(newWord);
+		if (newWord.toString().equals(word)) {
+			int[][] out = new int[word.length()][3];
+			for(int c = 0; c < word.length(); c++){
+				out[c] = new int[] {i + xdir * c, j+ydir * c, k+zdir * c};
+			}
+			return out;
+		}
+		return null;
+	}
+
+	/**
+	 * Given a starting position in the grid, checks if a "word" of the same length as the word that
+	 * is being searched for can fit within the grid without going out of bounds
+	 * @param vector the vector signaling which direction from the starting point to look
+	 * @param grid the grid that is being searched
+	 * @param word the word that is being searched for
+	 * @param i the x-component of the current position in the grid
+	 * @param j the y-component of the current position in the grid
+	 * @param k the z-component of the current position in the grid
+	 * @return true if the "word" can be searched for without going out of bounds, false otherwise
+	 */
+	private boolean valid(int[] vector, char[][][] grid, String word, int i, int j, int k){
+		int xdir = vector[0];
+		int ydir = vector[1];
+		int zdir = vector[2];
+		int i1 = i + xdir * (word.length() - 1);
+		int j1 = j + ydir * (word.length() - 1);
+		int k1 = k + zdir * (word.length() - 1);
+		if(i1 < 0 || i1 >= grid.length) return false;
+		if(j1 < 0 || j1 >= grid[0].length) return false;
+		if(k1 < 0 || k1 >= grid[0][0].length) return false;
+		return true;
+	}
+
+	/**
+	 * Searches for the specified word in the specified grid.
+	 * @param grid the grid of characters comprising the word search puzzle
+	 * @param word the word to search for
+	 * @return If the grid contains the
+	 * word, then the method returns a list of the (3-d) locations of its letters; if not,
+	 */
+	public int[][] search (char[][][] grid, String word) {
+		ArrayList<int[]> vectors = makeVectors();
+		for(int i = 0; i < grid.length; i++){
+			for(int j = 0; j < grid[0].length; j++){
+				for(int k = 0; k < grid[0][0].length; k++){
+					for(int[] item : vectors){
+						if(!valid(item, grid, word, i, j, k)) continue;
+						int[][] out = checkVector(grid, word, item, i, j, k);
+						if(out != null) return out;
+					}
+				}
+			}
+		}
+		return null;
+	}
+
+	/**
+	 * Tries to create a word search puzzle of the specified size with the specified
+	 * list of words.
+	 * @param words the list of words to embed in the grid
+	 * @param sizeX size of the grid along first dimension
+	 * @param sizeY size of the grid along second dimension
+	 * @param sizeZ size of the grid along third dimension
+	 * @return a 3-d char array if successful that contains all the words, or <tt>null</tt> if
+	 * no satisfying grid could be found.
+	 */
+	public char[][][] make (String[] words, int sizeX, int sizeY, int sizeZ) {
+		// TODO: implement me
+		char[][][] grid = new char[sizeX][sizeY][sizeX];
+
+		for (int i = 0; i < words.length; i++) {
+			System.out.println("count: " + i);
+		}
+
+		return new char[][][]{
+				{
+						{
+								0, 0
+						}
+				}
+		};
+	}
+
+	/**
+	 * Exports to a file the list of lists of 3-d coordinates.
+	 * You should not need to modify this method.
+	 * @param locations a list (for all the words) of lists (for the letters of each word) of 3-d coordinates.
+	 * @param filename what to name the exported file.
+	 */
+	public static void exportLocations (int[][][] locations, String filename) {
+		// First determine how many non-null locations we have
+		int numLocations = 0;
+		for (int i = 0; i < locations.length; i++) {
+			if (locations[i] != null) {
+				numLocations++;
+			}
+		}
+
+		try (final PrintWriter pw = new PrintWriter(filename)) {
+			pw.print(numLocations);  // number of words
+			pw.print('\n');
+			for (int i = 0; i < locations.length; i++) {
+				if (locations[i] != null) {
+					pw.print(locations[i].length);  // number of characters in the word
+					pw.print('\n');
+					for (int j = 0; j < locations[i].length; j++) {
+						for (int k = 0; k < 3; k++) {  // 3-d coordinates
+							pw.print(locations[i][j][k]);
+							pw.print(' ');
+						}
+					}
+					pw.print('\n');
+				}
+			}
+			pw.close();
+		} catch (FileNotFoundException fnfe) {
+			fnfe.printStackTrace();
+		}
+	}
+
+	/**
+	 * Exports to a file the contents of a 3-d grid.
+	 * You should not need to modify this method.
+	 * @param grid a 3-d grid of characters
+	 * @param filename what to name the exported file.
+	 */
+	public static void exportGrid (char[][][] grid, String filename) {
+		try (final PrintWriter pw = new PrintWriter(filename)) {
+			pw.print(grid.length);  // height
+			pw.print(' ');
+			pw.print(grid[0].length);  // width
+			pw.print(' ');
+			pw.print(grid[0][0].length);  // depth
+			pw.print('\n');
+			for (int x = 0; x < grid.length; x++) {
+				for (int y = 0; y < grid[0].length; y++) {
+					for (int z = 0; z < grid[0][0].length; z++) {
+						pw.print(grid[x][y][z]);
+						pw.print(' ');
+					}
+				}
+				pw.print('\n');
+			}
+			pw.close();
+		} catch (FileNotFoundException fnfe) {
+			fnfe.printStackTrace();
+		}
+	}
+
+	/**
+	 * Creates a 3-d word search puzzle with some nicely chosen fruits and vegetables,
+	 * and then exports the resulting puzzle and its solution to grid.txt and locations.txt
+	 * files.
+	 */
+	public static void main (String[] args) {
+		final WordSearch3D wordSearch = new WordSearch3D();
+		/*final String[] words = new String[] { "apple", "orange", "pear", "peach", "durian", "lemon", "lime", "jackfruit", "plum", "grape", "apricot", "blueberry", "tangerine", "coconut", "mango", "lychee", "guava", "strawberry", "kiwi", "kumquat", "persimmon", "papaya", "longan", "eggplant", "cucumber", "tomato", "zucchini", "olive", "pea", "pumpkin", "cherry", "date", "nectarine", "breadfruit", "sapodilla", "rowan", "quince", "toyon", "sorb", "medlar" };
+		final int xSize = 10, ySize = 10, zSize = 10;
+		final char[][][] grid = wordSearch.make(words, xSize, ySize, zSize);
+		exportGrid(grid, "grid.txt");
+
+		final int[][][] locations = wordSearch.searchForAll(grid, words);
+		exportLocations(locations, "locations.txt");*/
+
+
+
+
+		char[][][] test = {
+				{
+						{'a', 'a', 'a', 'a'},
+						{'o', 'a', 'a', 'a'},
+						{'a', 'a', 'a', 'a'}
+				},
+				{
+						{'a', 'a', 'a', 'a'},
+						{'o', 'a', 'a', 'a'},
+						{'a', 'a', 'a', 'a'}
+				}/*,
+				{
+						{'a', 'a', 'a', 'a'},
+						{'a', 'a', 'a', 'a'},
+						{'a', 'a', 'x', 'a'}
+				}*/
+		};
+		System.out.println(Arrays.deepToString(wordSearch.search(test, "oo")));
+
+		final char[][][] grid = new char[][][] {
+				{
+					{'m', 'n', 'o', 'p'},
+						{'p', 'r', 's', 't'},
+						{'u', 'v', 'w', 'x'},
+				}
+		};
+		//System.out.println(grid[0][1][0]);
+		//wordSearch.search(grid, "no");
+
+		//wordSearch.make(new String[] {"apple", "orange"}, 10, 10, 10);
+	}
+}
