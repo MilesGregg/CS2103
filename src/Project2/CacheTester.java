11faca173795b8a7300326b187ba33f24b198ff4
Miles <milesgregg3@gmail.com>
2020-11-02 10:23:52 -0500

    added project 2 files

---
+++ src/Project2/CacheTester.java
@@ -0,0 +1,16 @@
+package Project2;
+
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Code to test an <tt>LRUCache</tt> implementation.
+ */
+public class CacheTester {
+	@Test
+	public void leastRecentlyUsedIsCorrect () {
+		DataProvider<Integer,String> provider = null; // Need to instantiate an actual DataProvider
+		Cache<Integer,String> cache = new LRUCache<Integer,String>(provider, 5);
+	}
+}
